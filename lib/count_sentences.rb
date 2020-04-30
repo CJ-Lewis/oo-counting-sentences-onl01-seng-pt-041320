c2858bb25443067c35c0035e3624ab953d15b508
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-30 21:31:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    if String.end_with?(".")
+    if self.end_with?(".")
       return true
     else
       return false
@@ -11,7 +11,7 @@ class String
   end
 
   def question?
-    if String.end_with?("?")
+    if self.end_with?("?")
       return true
     else
       return false
