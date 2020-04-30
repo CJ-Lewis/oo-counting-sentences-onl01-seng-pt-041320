1656ae22b1b4f5d2936807c0d19b312a57fde3a8
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-30 21:23:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,17 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if String.end_with?(".")
+      return true
+    else
+      return false
   end
 
   def question?
-
+    if String.end_with?("?")
+      return true
+    else
+      return false
   end
 
   def exclamation?
